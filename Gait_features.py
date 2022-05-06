7259fce3ba6b3db631a8584b32bff68dbb7c7e97
Naima96 <naimamohabiad@gmail.com>
2022-05-06 18:42:38 +0200

    looking at data

--- Gait_features.py
+++ Gait_features.py
@@ -12,6 +12,7 @@ import scipy.io
 from utility_functions import calculate_norm_accandgyro
 import matplotlib.pyplot as plt
 import nolds as nld
+import os 
 
 def calculate_lyap(filename,step_vector,Numberofstrides,tao=10,dim=5):
 
@@ -77,18 +78,6 @@ def calculate_lyap(filename,step_vector,Numberofstrides,tao=10,dim=5):
         
     # return Normalize_periods,sampen_periods
 
-        
-        
-        
-        
-        
-        
-    
-    #calculate norm
-    #normalise signal from steps
-    #Crop at a number od steps
-    #Calculate 
-    
 
 def extract_walking_period(steps_vector,minimum_seperationwalkingbout=200,longest_walkingbout=60):
     
@@ -116,17 +105,17 @@ def extract_walking_period(steps_vector,minimum_seperationwalkingbout=200,longes
     
     
 
-def calculate_variability_activeperiod(steps_vector,minimum_seperationwalkingbout=100,longest_walkingbout=60):
+def calculate_variability_activeperiod(steps_vector,minimum_seperationwalkingbout=100,longest_walkingbout=60,remove_step=0):
     
 
     # group steps in same walking bouts
     consecutive_index_steps=_find_consecutive_index_ranges(steps_vector, increment = minimum_seperationwalkingbout)
     
-    print("found %d walking periods"%(len(consecutive_index_steps)))
+    # print("found %d walking periods"%(len(consecutive_index_steps)))
     
     # keep walking bouts with more than 60 steps
     consecutive_index_steps=[act for act in consecutive_index_steps if len(act)>=longest_walkingbout]
-    print("kept %d more than 60 steps"%(len(consecutive_index_steps)))
+    # print("kept %d more than 60 steps"%(len(consecutive_index_steps)))
     
     columns=['time_start','time_end','N_steps','stridetime_std','stridetime_Cov','steptime_std','steptime_Cov']
 
@@ -136,23 +125,44 @@ def calculate_variability_activeperiod(steps_vector,minimum_seperationwalkingbou
 
     for j in range(0,len(consecutive_index_steps)):
         variability=computeVarStride(fs=100,remove_outliers=True,N=3,use_smartstep=True,
-                                     manual_peaks=consecutive_index_steps[j],use_peaks=True,pocket=False,remove_step=0)
-
-        time_start=variability['detailed_steptime'][0,0]
-        time_end=variability['detailed_steptime'][-1,0]
+                                     manual_peaks=consecutive_index_steps[j],use_peaks=True,pocket=False,remove_step=remove_step)
         
-        if len(variability)>1:
-            data=[time_start,time_end,len(variability['detailed_steptime']),
-                  variability['stridetime_std'],variability['stridetime_Cov'],
-                  variability['steptime_std'],variability['steptime_Cov']]
+        try:
+
             
-            var_data=pd.DataFrame([data],columns=columns)
-            variability_data= pd.concat([variability_data,var_data])
-            
-            detailed_stridetime=np.concatenate((detailed_stridetime,variability['detailed_stridetime']))
-            
-            #append to dictionary 
-            
+            if len(variability)>1:
+                
+                if variability['detailed_steptime'][0,0]==variability['detailed_steptime'][0,0]:
+                
+                    time_start=variability['detailed_steptime'][0,0]
+                    time_end=variability['detailed_steptime'][-1,0]
+                    
+                    data=[time_start,time_end,len(variability['detailed_steptime']),
+                          variability['stridetime_std'],variability['stridetime_Cov'],
+                          variability['steptime_std'],variability['steptime_Cov']]
+                    
+                    var_data=pd.DataFrame([data],columns=columns)
+                    
+                    variability_data= pd.concat([variability_data,var_data])
+                    
+                    detailed_stridetime=np.concatenate((detailed_stridetime,variability['detailed_stridetime']))
+                    
+                    
+                else:
+                    
+                    data=[np.NAN,np.NAN,np.NAN,
+                          np.NAN,np.NAN,
+                          np.NAN,np.NAN]
+                    
+                    var_data=pd.DataFrame([data],columns=columns)
+                    
+                    variability_data= pd.concat([variability_data,var_data])
+                    
+                    
+                    
+                #append to dictionary 
+        except:
+            print("problem with analysis")
 
             
     return variability_data,detailed_stridetime
@@ -160,23 +170,11 @@ def calculate_variability_activeperiod(steps_vector,minimum_seperationwalkingbou
 
 def computeVarStride(fs=100,remove_outliers=True,N=1,use_smartstep=False,manual_peaks=[],
                      use_peaks=True,pocket=True,remove_step=0,round_data=True):
-    """
-    compute stride time 
-    :param int fs: sampling frequency
-    :param bool remove_outlier: whether to remove outliers
-    :param int N: Nxstandard deviation 
-    :param bool use_peaks: peaks are used as a mark that step happened
-    :param bool pocket: whether one stride time is calculated 
-    """
-    #note: we remove two steps from beginging and end
+
     cycle_tempparam = {}
 
-    peaks=manual_peaks
+    peaks=manual_peaks[remove_step:-remove_step]
 
-
-    #---if cellphone is in the hand or waist we can detect leading and contralateral foot stride time---
-    stride_time_leading=np.diff(peaks[::2])/fs
-    stride_time_contralateral=np.diff(peaks[1::2])/fs
 
     list_stride_time=[]
     list_step_time=[]
@@ -197,33 +195,17 @@ def computeVarStride(fs=100,remove_outliers=True,N=1,use_smartstep=False,manual_
         
 
     if remove_outliers:
-        stride_time_leading=np.array([i for i in stride_time_leading if i >= 0.8 and i <= 1.8])
-        stride_time_contralateral=np.array([i for i in stride_time_contralateral if i >= 0.8 and i <= 1.8])
-        try:
-            #---stride time leading foot---
-            mean=np.mean(stride_time_leading)
-            cut_off=N*np.std(stride_time_leading)
-            lower, upper =  mean- cut_off, mean + cut_off
-            cycle_tempparam['stride_time_leading'] = np.array([i for i in stride_time_leading if i > lower and i < upper])
-        except Exception:
-            print("no stride time left")
-
-        #---stride time contralateral foot---
-        try:
-            mean=np.mean(stride_time_contralateral)
-            cut_off=N*np.std(stride_time_contralateral)
-            lower, upper =  mean- cut_off, mean + cut_off
-            cycle_tempparam['stride_time_contralateral'] = np.array([i for i in stride_time_contralateral if i > lower and i < upper])
-        except Exception:
-            print("no stride time left")
+        
         #---step time---
         try:
+            list_step_time=np.vstack([i for i in list_step_time if i[2]>=0.3 and i[2]<=0.9])
             mean=np.mean(list_step_time[:,2])
             cut_off=N*np.std(list_step_time[:,2])
             lower, upper =  mean- cut_off, mean + cut_off
             cycle_tempparam['steptime'] = np.array([i for i in list_step_time[:,2] if i > lower and i < upper])
         except Exception:
             print("no step time left")
+            cycle_tempparam['steptime'] =np.array([np.NAN])
 
         try:
             list_stride_time=np.vstack([i for i in list_stride_time if i[2]>=0.8 and i[2]<=1.7])
@@ -233,25 +215,16 @@ def computeVarStride(fs=100,remove_outliers=True,N=1,use_smartstep=False,manual_
             list_stride_time=np.vstack([i for i in list_stride_time if i[2]>=lower and i[2]<=upper])
         except:
             print("no step time left")
-    else:
-        cycle_tempparam['stride_time_leading']=stride_time_leading
-        cycle_tempparam['stride_time_contralateral']=stride_time_contralateral
-        cycle_tempparam['steptime']=step_time
+            cycle_tempparam['stridetime'] =np.array([np.NAN])
+
 
     #---merge left right stride cycle
 
-    rl_stride=list_stride_time[:,2]
 
-    if len(rl_stride)>1:
-        cycle_tempparam['stridetime']=rl_stride
+    if len(list_stride_time[:,2])>1:
+        cycle_tempparam['stridetime']=list_stride_time[:,2]
         cycle_tempparam["detailed_stridetime"]=list_stride_time
         cycle_tempparam["detailed_steptime"]=list_step_time
-
-        cycle_tempparam['stride_time_leading_std']=np.around(np.std(cycle_tempparam['stride_time_leading']),decimals=3)
-        cycle_tempparam['stride_time_leading_Cov']=np.around(np.std(cycle_tempparam['stride_time_leading']*100)/np.mean(cycle_tempparam['stride_time_leading']),decimals=3)
-
-        cycle_tempparam['stride_time_contralateral_std']=np.around(np.std(cycle_tempparam['stride_time_contralateral']),decimals=3)
-        cycle_tempparam['stride_time_contralateral_Cov']=np.around(np.std(cycle_tempparam['stride_time_contralateral']*100)/np.mean(cycle_tempparam['stride_time_contralateral']),decimals=3)
 
         cycle_tempparam['stridetime_std']=np.around(np.std(cycle_tempparam['stridetime']),decimals=3)
         cycle_tempparam['stridetime_Cov']=np.around(np.std(cycle_tempparam['stridetime']*100)/np.mean(cycle_tempparam['stridetime']),decimals=3)
@@ -262,21 +235,157 @@ def computeVarStride(fs=100,remove_outliers=True,N=1,use_smartstep=False,manual_
 
     else:
         print("most strides have been filtered because of misdetection ")
+        cycle_tempparam['stridetime']=np.array([np.NAN])
+        cycle_tempparam["detailed_stridetime"]=np.array([[np.NAN,np.NAN],[np.NAN,np.NAN]])
+        cycle_tempparam["detailed_steptime"]=np.array([[np.NAN,np.NAN],[np.NAN,np.NAN]])
+
+        cycle_tempparam['stridetime_std']=np.NAN
+        cycle_tempparam['stridetime_Cov']=np.NAN
+
+        cycle_tempparam['steptime_std']=np.NAN
+        cycle_tempparam['steptime_Cov']=np.NAN
+
+
+        
+        
+                
     cycle_temp=cycle_tempparam
     return cycle_temp
+
+
+def read_faller_excel(subjects):
+    file_name="ST_Falls_unblinded_Naima.xlsx"
+    faller_excel=pd.read_excel(file_name,usecols="A,E")
+    subjects_names=result.subject.values
+    
+    s=subjects_names[0]
+    faller=[]
+    
+    
+    for s in subjects_names:
+        
+        f=faller_excel.loc[faller_excel['StudyID']==s.replace(" ", ""),'N_falls_year1'].values
+        if len(f)>=1:
+            if f>=1:
+            
+                faller.append([1])
+            else:
+                faller.append([0])
+            
+        else:
+            faller.append([np.NAN])
+            
+    faller=np.vstack(faller)
+    
+    result["prosp_falls"]=faller
+    
 
 if __name__=="__main__":
     # subj1_steps=np.load('subj1_steps.npy')
     # var_s1_dff,stride_s1_dff=calculate_variability_activeperiod(subj1_steps,minimum_seperationwalkingbout=200,longest_walkingbout=60)
     
+    columns=["subject","No. bouts","No. steps","short percen",
+             "med percen","long percen","SD short","SD med","SD long",
+             "Cov short","Cov med","Cov long"]
     
+    direct_path="results"
     
-    subj2_steps=np.load('JBK411_steps.npy')
-    var_s2_df,stride_s2_df=calculate_variability_activeperiod(subj2_steps,minimum_seperationwalkingbout=200,longest_walkingbout=60)
+    files=[f for f in os.listdir(direct_path) if os.path.isfile(os.path.join(direct_path,f))]
+    result_list=[]
     
+    short=60
+    mid=100
+    long=200
+
+    for f in files:
+        
+        if f.endswith(".npy"):
+        
+            filepath=direct_path +"//" +f
+    
+            subj2_steps=np.load(filepath)
+            var_s2_df,stride_s2_df=calculate_variability_activeperiod(subj2_steps,minimum_seperationwalkingbout=200,longest_walkingbout=short)
+        
+            subject_file=f[:-10]
+            number_walking_bout= len(var_s2_df)
+            total_number_steps= np.sum(var_s2_df["N_steps"].values)
+            
+            percen_short= np.sum(var_s2_df["N_steps"].values<mid)*100/number_walking_bout
+            
+            percen_med=np.sum((var_s2_df["N_steps"].values>mid) & (var_s2_df["N_steps"].values<long))*100/number_walking_bout
+            
+            percen_long= np.sum(var_s2_df["N_steps"].values>long)*100/number_walking_bout
+            
+            var_short_SD=np.median(var_s2_df.loc[var_s2_df["N_steps"]<mid,"stridetime_std"].values)
+            
+            var_med_SD=np.median(var_s2_df.loc[(var_s2_df["N_steps"]>mid) & 
+                                                  (var_s2_df["N_steps"]<long) ,"stridetime_std"].values)
+            
+            
+            var_long_SD=np.median(var_s2_df.loc[var_s2_df["N_steps"]>long,"stridetime_std"].values)
+            
+            
+            var_short_Cov=np.median(var_s2_df.loc[var_s2_df["N_steps"]<mid,"stridetime_Cov"].values)
+            
+            var_med_Cov=np.median(var_s2_df.loc[(var_s2_df["N_steps"]>mid) & 
+                                                  (var_s2_df["N_steps"]<long) ,"stridetime_Cov"].values)
+            
+            
+            var_long_Cov=np.median(var_s2_df.loc[var_s2_df["N_steps"]>long,"stridetime_Cov"].values)
+            
+            
+            result_list.append([subject_file,number_walking_bout,
+                                total_number_steps,percen_short,percen_med,percen_long,
+                                var_short_SD,var_med_SD,var_long_SD,var_short_Cov,var_med_Cov,var_long_Cov])
+            
+
+    result=pd.DataFrame(data=result_list, columns=columns)
     # var_s2_df.to_excel('result_JBK411.xlsx')
     # filename="d://Users//al-abiad//Desktop//kim//Naima & Thomas//JBK411.mat"
     # calculate_lyap(filename,subj2_steps,Numberofstrides=100,tao=10,dim=5)
     
+    # result.to_excel("result.xlsx")
     
+    subjects_names=result.subject.values
+    
+    file_name="ST_Falls_unblinded_Naima.xlsx"
+    faller_excel=pd.read_excel(file_name,usecols="A,E")
+    subjects_names=result.subject.values
+    
+    s=subjects_names[0]
+    faller=[]
+    
+    
+    for s in subjects_names:
+        
+        f=faller_excel.loc[faller_excel['StudyID']==s.replace(" ", ""),'N_falls_year1'].values
+        if len(f)>=1:
+            if f>=1:
+            
+                faller.append([1])
+            else:
+                faller.append([0])
+            
+            
+        else:
+            faller.append([np.NAN])
+            
+    faller=np.vstack(faller)
+    
+    result["prosp_falls"]=faller
+    
+    
+    result = result[result['prosp_falls'].notna()]
+    
+    print("There are %d fallers "%np.sum(result['prosp_falls'].values))
+    
+    
+    print("There are %d nonfallers "%(len(result['prosp_falls'].values)-np.sum(result['prosp_falls'].values)))
+    
+    
+    import seaborn as sns
+    for c in result.columns[1:-1]:
+        plt.figure()
+        sns.boxplot(x="prosp_falls", y=c,
+                          data=result)
     
---
+++ clustering.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 13:20:01 2022
+
+@author: al-abiad
+"""
